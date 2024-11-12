5f76b133d95aa49e3321fb4c487ad79d997f7426
Szántó Benjámin <szantobenjamin97@gmail.com>
2024-11-12 19:50:06 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,39 +1,43 @@
-const kijelzo = document.getElementById("kijelzo");
-const kep = document.getElementById("kep");
-const szoveg = document.getElementById("szoveg");
+const kep = document.getElementById('kep');
+const hatter = document.getElementById('hatter');
+const szoveg = document.getElementById('szoveg');
 
-let isClosed = true;
-
+let zarva = true;
 const uzik = [
     "A BELSŐ SZÁMÍT",
     "A CSILLAGOK FIGYELNEK",
     "A DOLGOK JÓL HALADNAK",
     "VIDÁMSÁG! :)",
-    "VIDD VÉGIG AZ ÖTLETEIDET!",
+    "VIDD VÉGIG AZ ÖTLETEIDET!", 
     "VIGYÉL EGY JÓ HÍRT!"
 ];
 
-kijelzo.addEventListener("click", async () => {
-    if (isClosed) {
-        for (let index = 0; index < 11; index++) {
-            await new Promise(resolve => setTimeout(resolve, 13));
-            kep.src = `images/bottlecap_${index}.png`;
-            if (index == 10) {
-                const randomIndex = Math.floor(Math.random() * uzik.length);
-                szoveg.textContent = uzik[randomIndex];
-            }
-        }
-        isClosed = false;
-    }
-    else {
-        for (let index = 10; index >= 0; index--) {
-            await new Promise(resolve => setTimeout(resolve, 13));
-            kep.src = `images/bottlecap_${index}.png`;
-            if (index == 10) {
-                szoveg.textContent = "";
-            }
-        }
-        isClosed = true;
-    }
+// Cache létrehozása oldalbetöltéskor
+const images = []; // Ebbe tároljuk a betöltött képeket
 
+document.addEventListener('DOMContentLoaded', () => {
+    for (let i = 0; i <= 10; i++) {
+        const img = new Image();
+        img.src = `images/bottlecap_${i}.png`; // Kép forrása
+        images.push(img); // Cache-eljük a képet
+    }
+});
+
+hatter.addEventListener('click', async function() {
+    if (zarva === true) {
+        for (let index = 0; index <= 10; index++) {
+           kep.src = images[index].src; // Már előre cache-elt képet használunk
+           await new Promise(pr => setTimeout(pr, 13)); // Késleltetési idő
+        }
+        const randomIndex = Math.floor(Math.random() * uzik.length); // [0,5]
+        szoveg.textContent = uzik[randomIndex];
+        zarva = false;
+    } else {
+        szoveg.textContent = "";
+        for (let index = 10; index >= 0; index--) {
+            kep.src = images[index].src; // Már előre cache-elt képet használunk
+            await new Promise(pr => setTimeout(pr, 13)); // Késleltetési idő
+        }
+        zarva = true;
+    }   
 });
